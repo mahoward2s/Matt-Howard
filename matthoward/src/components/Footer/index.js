20cd87876b993d46bad553dc57bc616e54b05a20
Sportsguy <mahoward2s@gmail.com>
2021-04-06 18:48:05 -0500

    made changes to footer layout

--- matthoward/src/components/Footer/index.js
+++ matthoward/src/components/Footer/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar"
 import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <span>Pupster 2017</span>
-    </footer>
+<Navbar bg="dark" variant="dark" expand="lg" fixed="bottom">
+  <Navbar.Brand id="footer">Website By: Mahoward2s</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+</Navbar>
   );
 }
 
