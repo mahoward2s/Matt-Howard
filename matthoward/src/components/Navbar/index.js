4cbe2c2ce29c5c39d79c28868cf7677e8c733a92
Sportsguy <mahoward2s@gmail.com>
2021-04-06 13:26:57 -0500

    deleted out deneric files, and added code to navbar

---
+++ matthoward/src/components/Navbar/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        Matthew Howard
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/about"
+              className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
+            >
+              About
+            </Link>
+          </li>
+          {/* How to make a drop down arrow menu with bootstrap */}
+{/*           <li className="nav-item">
+            <Link
+              to="/discover"
+              className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
+            >
+              Discover
+            </Link>
+          </li> */}
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
