114e4b0e217122074e75011c15b05638a3dd3da2
Sportsguy <mahoward2s@gmail.com>
2021-04-06 18:27:31 -0500

    installed reactbootstrap, and changed layout of navbar

--- matthoward/src/components/Navbar/index.js
+++ matthoward/src/components/Navbar/index.js
@@ -1,60 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar"
 import "./style.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-function Navbar() {
+function NavigationBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <Link className="navbar-brand" to="/">
-        Matthew Howard
-      </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/about"
-              className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
-            >
-              About
-            </Link>
-          </li>
-{/*           <li className="nav-item dropdown">
-            <Link
-              to="/discover"
-              className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
-            >
-              Discover
-            </Link>
-          </li> */}
-          <li className="nav-item">
-            <Link
-              to="/contact"
-              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
   );
 }
 
-export default Navbar;
-
-
-
-
-<Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
-    Portfolio
-  </Dropdown.Toggle>
-
-  <Dropdown.Menu>
-    <Dropdown.Item href="#/action-1">Collaborations</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Applications</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Websites</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Personal Projects</Dropdown.Item>
-  </Dropdown.Menu>
-</Dropdown>
+export default NavigationBar;
