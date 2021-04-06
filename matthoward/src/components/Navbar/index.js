5a7de63f6531b45fd86df1cfe4b5a297cd8a5d8f
Sportsguy <mahoward2s@gmail.com>
2021-04-06 18:34:01 -0500

    manipulating navbar

--- matthoward/src/components/Navbar/index.js
+++ matthoward/src/components/Navbar/index.js
@@ -5,20 +5,21 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function NavigationBar() {
   return (
-<Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+<Navbar bg="dark" expand="lg">
+  <Navbar.Brand href="#home">Matt Howard</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+      <Nav.Link href="#link">About</Nav.Link>
+      <NavDropdown title="Portfolio" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Collaboration</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Applications</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Websites</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.4">Personal Projects</NavDropdown.Item>
       </NavDropdown>
+      <Nav.Link href="#link">Contact</Nav.Link>
     </Nav>
     <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
