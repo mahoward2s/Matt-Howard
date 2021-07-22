bf21ddf74a7b61f13896374cc4de4db69c698e4f
Sportsguy <mahoward2s@gmail.com>
2021-07-22 15:40:35 -0500

    changed buttons on contact page

--- matthoward/src/pages/Contact.js
+++ matthoward/src/pages/Contact.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Card, ListGroup, Button, Container, Row, Col } from 'react-bootstrap';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab, fagithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare, faLinkedin, faGoogle, faFacebookSquare, faInstagramSquare } from '@fortawesome/free-brands-svg-icons'
+import {faEnvelopeOpenText, faPhoneVolume, faFilePdf} from '@fortawesome/free-solid-svg-icons'
 
 function Contact() {
   return (
@@ -19,9 +20,9 @@ function Contact() {
             style={{ width: '40rem' }}>
             <Card.Img
               variant="top"
-              src="images/IMG_Phones.jpg" 
+              src="images/IMG_Phones.jpg"
               alt="image of payphones"
-              />
+            />
             <Card.Body>
               <Card.Title>Matthew Howard</Card.Title>
               <Card.Text>
@@ -37,10 +38,11 @@ function Contact() {
           <Col>
             <Card
               className="text-center"
+              border="dark"
               style={{ width: '18rem' }}>
               <Card.Img variant="top"
-              alt="image of github profile"
-              src="images/emojime.jpeg"/>
+                alt="image of github profile"
+                src="images/emojime.jpeg" />
               <Card.Body>
                 <Card.Title>Profiles:</Card.Title>
                 <Card.Text>
@@ -49,13 +51,14 @@ function Contact() {
                 <Button
                   href="https://github.com/mahoward2s"
                   target="_blank"
-                  variant="primary">GitHub
-                  <fagithub />
+                  variant="primary">
+                    <FontAwesomeIcon icon={faGithubSquare} />
                 </Button>
                 <Button
                   href="https://www.linkedin.com/in/matthew-h-974498108/"
                   target="_blank"
-                  variant="info">LinkedIn
+                  variant="info">
+                    <FontAwesomeIcon icon={faLinkedin} />
                 </Button>
               </Card.Body>
             </Card>
@@ -63,24 +66,26 @@ function Contact() {
           <Col>
             <Card
               className="text-center"
+              border="dark"
               style={{ width: '18rem' }}>
               <Card.Img variant="top"
-              alt="Call Me Image"
-              src="images/CallMe.png"/>
+                alt="Call Me Image"
+                src="images/CallMe.png" />
               <Card.Body>
                 <Card.Title>Contact:</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                  Below is my personal email and phone number.  Feel free to reach out to discuss our next project.
                 </Card.Text>
                 <Button
                   href="mailto:<nowiki>mahoward2s@gmail.com?subject=' Contact From Portfolio'"
                   target="_blank"
-                  variant="secondary">Email
+                  variant="secondary">
+                    <FontAwesomeIcon icon={faEnvelopeOpenText} />
                 </Button>
                 <Button
                   href="tel:3146245710"
-                  variant="secondary">Phone
+                  variant="secondary">
+                    <FontAwesomeIcon icon={faPhoneVolume} />
                 </Button>
               </Card.Body>
             </Card>
@@ -88,27 +93,29 @@ function Contact() {
           <Col>
             <Card
               className="text-center"
+              border="dark"
               style={{ width: '18rem' }}>
-              <Card.Img variant="top" 
-              alt="Image of SEMO, LU, and WUSTL"
-              src="images/Colleges.png"/>
+              <Card.Img variant="top"
+                alt="Image of SEMO, LU, and WUSTL"
+                src="images/Colleges.png" />
               <Card.Body>
                 <Card.Title>Resume:</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                  Below you can choose Google Docs or PDF for a copy of my resume.
                 </Card.Text>
                 <Button
                   href="https://docs.google.com/document/d/1PRWn6gj7CNjXeJ8znhUkrbMLis3eGa6DaqXzQsRpPe0/edit?usp=sharing"
                   target="_blank"
-                  variant="success">Google
+                  variant="success">
+                    <FontAwesomeIcon icon={faGoogle} />
                 </Button>
                 <Button
                   variant="danger"
                   href="./public/images/Matt_Howard_Resume.pdf"
                   target="_blank" class="btn btn-warning"
                   download><i
-                    class="fa fa-download"></i>PDF
+                    class="fa fa-download"></i>
+                    <FontAwesomeIcon icon={faFilePdf} />
                 </Button>
               </Card.Body>
             </Card>
@@ -116,11 +123,12 @@ function Contact() {
           <Col>
             <Card
               className="text-center"
+              border="dark"
               style={{ width: '18rem' }}>
-              <Card.Img 
-              variant="top" 
-              alt="image of matt with dalmatian"
-              src="images/FacebookProfile.jpg"/>
+              <Card.Img
+                variant="top"
+                alt="image of matt with dalmatian"
+                src="images/FacebookProfile.jpg" />
               <Card.Body>
                 <Card.Title>Social Media:</Card.Title>
                 <Card.Text>
@@ -129,22 +137,29 @@ function Contact() {
                 <Button
                   href="https://www.instagram.com/mhlth282"
                   target="_blank"
-                  variant="dark">Personal
+                  variant="dark">
+                    <FontAwesomeIcon icon={faInstagramSquare} />
+                    Personal
                 </Button>
                 <Button
                   href="https://www.instagram.com/depression.art.inc"
                   target="_blank"
-                  variant="dark">Art
+                  variant="dark">
+                    <FontAwesomeIcon icon={faInstagramSquare} />
+                    Art
                 </Button>
                 <Button
                   href="https://www.instagram.com/bluescollector2.0"
                   target="_blank"
-                  variant="dark">Sports
+                  variant="dark">
+                    <FontAwesomeIcon icon={faInstagramSquare} />
+                    Sports
                 </Button>
                 <Button
                   href="https://www.facebook.com/mahoward2s/"
                   target="_blank"
-                  variant="dark">Facebook
+                  variant="dark">
+                    <FontAwesomeIcon icon={faFacebookSquare} />
                 </Button>
               </Card.Body>
             </Card>
