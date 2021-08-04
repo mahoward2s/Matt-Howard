ed22fc92ff6a0225de80d1820ed905f1457e3fba
Sportsguy <mahoward2s@gmail.com>
2021-08-04 09:26:09 -0500

    added images to personal projects, and rearranged items, and added styling

--- matthoward/src/pages/PersonalProjects.js
+++ matthoward/src/pages/PersonalProjects.js
@@ -6,61 +6,123 @@ function PersonalProjects() {
     <div>
       <Container>
         <Row className="justify-content-center">
-          <h1>Personal Projects</h1>
-        </Row>
-        <Row className="justify-content-center">
           <h1>Graphic Design</h1>
         </Row>
         <Row className="justify-content-center">
-          <Carousel fade>
-            <Carousel.Item interval={1000}>
+          <Carousel>
+            <Carousel.Item interval={1500}>
               <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=First slide&bg=373940"
-                alt="First slide"
+                src="images/O'reilly 2020 Allstar Nameplate.png"
+                alt="Oreilly Nameplate"
               />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item interval={1500}>
               <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
-                alt="Second slide"
+                src="images/HUBERDEAU 2020 Allstar Nameplate.png"
+                alt="Hubderdeau Nameplate"
               />
-
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item interval={1500}>
               <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
+                src="images/Panarin Nameplate.png"
+                alt="Panarin Nameplate"
               />
-
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-100"
+                src="images/Perron Winter Classic Nameplate.png"
+                alt="Perron Nameplate"
+              />
+            </Carousel.Item>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-100"
+                src="images/Bozak SCF Nameplate.png"
+                alt="Bozak Nameplate"
+              />
+            </Carousel.Item>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-100"
+                src="images/Schenn Stadium SeriesNameplate.png"
+                alt="Schenn Stadium Series Nameplate"
+              />
+            </Carousel.Item>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-100"
+                src="images/Retro Blais Nameplate.png"
+                alt="Blais Retro Nameplate"
+              />
+            </Carousel.Item>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-100"
+                src="images/Blais Nameplate.png"
+                alt="Blais Nameplate"
+              />
+            </Carousel.Item>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-100"
+                src="images/Sobotka Nameplate.png"
+                alt="Sobotka Nameplate"
+              />
+            </Carousel.Item>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-100"
+                src="images/Krug Bubble Nameplate.png"
+                alt="Krug Bubble Nameplate"
+              />
+            </Carousel.Item>
+            <Carousel.Item interval={1500}>
+              <img
+                className="d-block w-100"
+                src="images/Porter 38 Nameplate.png"
+                alt="Porter 38 Nameplate"
+              />
             </Carousel.Item>
           </Carousel>
         </Row>
         <Row className="justify-content-center">
-          <h1>File Maker-Coming Soon</h1>
+          <img
+            src="images/Six Crosses.png"
+            alt="Six Crosses Business Card Logo"
+            style={{ 
+              width: '25rem',
+              padding: '15px'
+            }}
+          />
+          <img
+            src="images/Six Cross Design Logo.png"
+            alt="Six Cross Design Logo"
+            style={{ 
+              width: '25rem',
+              padding: '15px'
+          }}
+          />
+          <img
+            className="d-block w-100"
+            src="images/Six Cross Vault.png"
+            alt="Six Cross Vault Landing Page"
+          />
         </Row>
         <Row className="justify-content-center">
-          <h1>Websites-Coming Soon</h1>
+          <h3>File Maker-Coming Soon</h3>
         </Row>
         <Row className="justify-content-center">
-          <h1>Applications-Coming Soon</h1>
+          <h3>Websites-Coming Soon</h3>
         </Row>
         <Row className="justify-content-center">
-          <h1>WordPress-Coming Soon</h1>
+          <h3>Applications-Coming Soon</h3>
+        </Row>
+        <Row className="justify-content-center">
+          <h3>WordPress-Coming Soon</h3>
         </Row>
       </Container>
     </div>
